5d68861322ff4d2a9e84e4bdd685a5b327dcf710
Ananas <83706294+R3tr0Ananas@users.noreply.github.com>
2023-04-02 18:56:55 +0200

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,4 @@
+INSERT INTO items (NAME, label, weight, rare, can_remove) VALUES 
+	("coughmedicine", "Cough Medicine", 1, 0, 1),
+	("nauesamedicine", "Nausea medicine", 1, 0, 1),
+	("gingermedicine", "Ginger Medicine", 1, 0, 1); 
